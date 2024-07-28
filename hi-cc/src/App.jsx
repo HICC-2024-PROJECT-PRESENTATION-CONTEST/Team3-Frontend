2681b137c195f8812598aef4d52160c245ea1517
seoyeon5117 <seoyun020207@gmail.com>
2024-07-28 18:13:57 +0900

    design: App.css 설정

--- hi-cc/src/App.jsx
+++ hi-cc/src/App.jsx
@@ -1,4 +1,5 @@
-import { Routes, Route, BrowserRouter, Navigate } from "react-router-dom";
+import { Routes, Route, BrowserRouter } from "react-router-dom";
+import './App.css';
 
 import AdminLogin from './pages/adminPage/AdminLogin/AdminLogin';
 import QR from './pages/adminPage/QR/QR';
