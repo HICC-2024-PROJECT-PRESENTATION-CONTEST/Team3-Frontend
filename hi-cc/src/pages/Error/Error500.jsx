2df3162e4818c77c9ccbd09816283fe55a4f1fe0
seoyeon5117 <seoyun020207@gmail.com>
2024-07-31 22:09:44 +0900

    chore: wrapper 이름 수정

--- hi-cc/src/pages/Error/Error500.jsx
+++ hi-cc/src/pages/Error/Error500.jsx
@@ -4,16 +4,16 @@ import ErrorModal from "../../components/ErrorModal";
 
 export default function Error500() {
     return (
-        <NotFoundWrapper>
+        <ServerErrorWrapper>
             <ErrorModal title="500" subtitle="Internal Server Error">
                 서비스 이용에 불편을 드려 죄송합니다.<br />
                 관리자에게 문의하시거나 잠시 후 다시 시도하세요.
             </ErrorModal>
-        </NotFoundWrapper>
+        </ServerErrorWrapper>
     )
 };
 
-const NotFoundWrapper = styled.div`
+const ServerErrorWrapper = styled.div`
     display: flex;
     position: relative;
     width: 100vw;
