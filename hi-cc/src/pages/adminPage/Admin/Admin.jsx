940f27e06b4a305b4aeeba6f2e046ae94f8f4c54
seoyeon5117 <seoyun020207@gmail.com>
2024-08-05 23:57:47 +0900

    feat: admin 페이지 구현(사용자 관리 페이지, QR 코드 페이지 이동)

--- hi-cc/src/pages/adminPage/Admin/Admin.jsx
+++ hi-cc/src/pages/adminPage/Admin/Admin.jsx
@@ -1,6 +1,50 @@
+import { useNavigate } from "react-router-dom";
+import styled from "styled-components";
+
 export default function Manager() {
+    const navigate = useNavigate();
+
+    function handleClick(nextPage) {
+        navigate(nextPage);
+    }
+
     return(
-        <></>
+        <ManagerWrapper>
+            <Button onClick={() => handleClick('/admin/users')}>사용자 관리</Button>
+            <Button onClick={() => handleClick('/admin/qr')}>QR 코드</Button>
+        </ManagerWrapper>
     )
 };
 
+const ManagerWrapper = styled.div`
+    display: flex;
+    flex-direction: column;
+
+    width: 100vw;
+    height: 100vh;
+
+    justify-content: center;
+    align-items: center;
+
+    background-color: #2d2b39;
+`
+
+const Button = styled.button`
+    width: 200px;
+    height: 50px;
+    
+    border: 0px;
+    border-radius: 10px;
+    margin: 10px 0;
+    
+    font-size: 20px;
+    color: #FFFFFF;
+
+    background-color: #161616;
+
+    cursor: pointer;
+
+    &:hover {
+        background-color: #000000;
+    }
+`
