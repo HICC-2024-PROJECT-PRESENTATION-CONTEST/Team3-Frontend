f672f7dfdfaaa3a58f16b3ffaeb0642b6f9b76b6
seoyeon5117 <seoyun020207@gmail.com>
2024-08-09 17:46:35 +0900

    feat: 로그인 페이지 구현

--- hi-cc/src/pages/userPage/Login/Login.jsx
+++ hi-cc/src/pages/userPage/Login/Login.jsx
@@ -1,7 +1,181 @@
+import { useState, useEffect } from "react";
+import { useNavigate } from "react-router-dom";
+import styled from "styled-components";
+
+import Button from "../../../components/MainButton";
+
+const API_URL = import.meta.env.VITE_API_URL;
+
 export default function Login() {
-    return(
-        <>
-            <button>로그인하기</button>
-        </>
+    const navigate = useNavigate();
+    const [phonenumber, setPhonenumber] = useState("");
+    const [password, setPassword] = useState("");
+    const [isValid, setIsValid] = useState(false);
+
+    useEffect(() => {
+        const validPhone = (phonenumber.length === 11);
+        const validPassword = (password.length >= 4);
+        setIsValid(validPhone && validPassword);
+    }, [phonenumber, password])
+
+    function handlePhonenumberChange(e) {
+        const numbersOnly = e.target.value.replace(/\D/g, '');
+        setPhonenumber(numbersOnly);
+    }
+
+    function formatPhoneNumber(value) {
+        // 숫자만 추출
+        const numbersOnly = value.replace(/\D/g, '');
+
+        // 하이픈 추가
+        if (numbersOnly.length > 3 && numbersOnly.length <= 7) {
+            return numbersOnly.replace(/(\d{3})(\d+)/, "$1-$2");
+        } else if (numbersOnly.length > 7) {
+            return numbersOnly.replace(/(\d{3})(\d{4})(\d+)/, "$1-$2-$3");
+        }
+        return numbersOnly;
+    }
+
+    function handlePasswordChange(e) {
+        setPassword(e.target.value);
+    }
+
+    function handleClick() {
+        fetchLogin();
+    }
+
+    async function fetchLogin(){
+        const data = {
+            phone: phonenumber,
+            password: password,
+        }
+        await fetch(`${API_URL}/auth/login`, {
+            method: 'POST',
+            headers: {
+                'Content-Type': 'application/json',
+            },
+            body: JSON.stringify(data),
+        })
+        .then((response) => {
+            if(!response.ok){
+                throw new Error('error');
+            } else {
+                navigate('/mypage');
+            }
+        })
+        .catch((error) => {
+            console.error(error.message);
+        });
+    }
+
+    function handleKeyDown(e) {
+        if (e.key === "Enter") {
+            handleClick();
+        }
+    }
+
+    return (
+        <LoginWrapper>
+            <LoginInnerWrapper>
+                <Title>
+                    <TitleText>로그인하기</TitleText>
+                    <DescriptionText>
+                        전화번호와 비밀번호를 입력해주세요.
+                    </DescriptionText>
+                </Title>
+                <Input
+                    type="text"
+                    name="phonenumber"
+                    value={formatPhoneNumber(phonenumber)}
+                    onChange={handlePhonenumberChange}
+                    onKeyDown={(e) => ["e", "E", "+", "-"].includes(e.key) && e.preventDefault()} // e, E, +, - 입력 막기
+                    placeholder="전화번호"
+                    maxLength="13"
+                />
+                <Input
+                    type="password"
+                    name="password"
+                    value={password}
+                    onChange={handlePasswordChange}
+                    onKeyDown={handleKeyDown} // enter 키 처리
+                    placeholder="비밀번호" />
+                <DescriptionText2>
+                    비밀번호를 잊어버리셨다면 관리자에게 문의해주세요.
+                </DescriptionText2>
+            </LoginInnerWrapper>
+            <Button onClick={handleClick} $valid={isValid} $position="absolute">로그인</Button>
+        </LoginWrapper>
     )
 };
+
+const LoginWrapper = styled.div`
+    position: relative;
+    display: flex;
+    flex-direction: column;
+    align-items: center;
+    width: 100vw;
+    height: 100vh;
+    background: #F9DBDD;
+    overflow: auto;
+`
+
+const LoginInnerWrapper = styled.div`
+    position: relative;
+    display: flex;
+    flex-direction: column;
+    align-items: center;
+    width: 75vw;
+    min-width: calc(230px -5vw);
+    max-width: calc(480px - 5vw);
+    height: auto;
+    min-height: 60vh;
+`
+
+const Title = styled.span`
+    width: 100%;
+    margin: 11vh 0 3vh 0;
+    top: 100px;
+`
+
+const TitleText = styled.div`
+    margin: 1vh 0;
+    font-size: 25px;
+`
+
+const DescriptionText = styled.div`
+    font-size: 13px;
+    color: #464646;
+`
+
+const Input = styled.input`
+    width: calc(100% - 30px);
+    height: 45px;
+
+    margin: 10px;
+    padding: 0 15px;
+
+    border: solid 3px #000000;
+    border-radius: 10px;
+
+    font-size: 15px;
+
+    &::placeholder {
+        color: #8F8F8F;
+    }
+
+    // type이 number일 경우 화살표 제거
+    &::-webkit-outer-spin-button {
+        -webkit-appearance: none;
+        margin: 0;
+    }
+    &::-webkit-inner-spin-button {
+        -webkit-appearance: none;
+        margin: 0;
+    }
+`
+
+const DescriptionText2 = styled.div`
+    width: calc(100% - 20px);
+    font-size: 12px;
+    color: #8F8F8F;
+`
