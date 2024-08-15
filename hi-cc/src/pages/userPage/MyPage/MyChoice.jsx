9c98f58f33ce6155f57bda2e53bbbab2142224e9
seoyeon5117 <seoyun020207@gmail.com>
2024-08-15 21:31:22 +0900

    feat: 내 선택 상대방 선택 프로필 불러오기

--- hi-cc/src/pages/userPage/MyPage/MyChoice.jsx
+++ hi-cc/src/pages/userPage/MyPage/MyChoice.jsx
@@ -1,14 +1,60 @@
-import { useEffect } from "react";
+import { useState, useEffect } from "react";
 import styled from "styled-components";
+
+import ProfileCard from "../../../components/MyPageProfileCard";
 
 const API_URL = import.meta.env.VITE_API_URL;
 
 export default function MyChoice() {
+    const [data, setData] = useState(null);
+
+    useEffect(() => {
+        fetchChoiceTo();
+    }, []);
+
+    // 나의 선택 가져오기
+    async function fetchChoiceTo() {
+        await fetch(`${API_URL}/profiles/@me/choices`, {
+            method: 'GET',
+            credentials: 'include',
+            headers: {
+                'Content-Type': 'application/json' 
+            }
+        })
+        .then((response) => {
+            if(!response.ok)
+                throw { status: response.status, message: response.statusText }
+            else return response.json();
+        })
+        .then((result) => {
+            setData(result.data.choices_to);
+        })
+        .catch((error) => {
+            if(error.status === 403) {
+                alert('접근 권한이 없습니다.');
+                navigate('/');
+            } else if(error.status === 404) {
+                alert('상대의 프로필 정보를 찾을 수 없습니다.');
+            } else if(error.status === 500 || error.status === 502) {
+                navigate('/500');
+            } else {
+                console.error(error);
+            }
+        })
+    }
+
     return(
         <MyChoiceWrapper>
-            <ProfileCard>
-                내가 선택한 상대방
-            </ProfileCard>
+            <ProfileCardWrapper>
+                {data ?
+                    (data.length !== 0) ? data.map((data) => {
+                        return <ProfileCard id={data.uid} key={data.uid} data={data} $delete={data.delete}/>
+                    })
+                        : <Error>내가 선택한 상대가 없습니다.</Error>
+                     :
+                    <Error>정보를 불러올 수 없습니다.</Error>
+                }
+            </ProfileCardWrapper>
         </MyChoiceWrapper>
     )
 };
@@ -19,15 +65,25 @@ const MyChoiceWrapper = styled.div`
     align-items: center;
     width: 100%;
     height: auto;
+    flex-grow: 1;
 `
 
-const ProfileCard = styled.div`
+const ProfileCardWrapper = styled.div`
     width: calc(100% - 30px);
     height: auto;
     margin: 20px 0px;
+    display: flex;
+    justify-content: center;
+`
+
+const Error = styled.div`
+    display: flex;
+    align-items: center;
+    width: auto;
+    height: 20px;
+
+    padding: 10px;
 
     background-color: #FFDEE2;
-
-    border: solid 3px #000000;
     border-radius: 10px;
 `
