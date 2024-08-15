27c74195c2635a9d57d6675496c821f978030025
Wany <sung@wany.io>
2024-08-16 02:11:42 +0900

    chore: 깃허브 액션 세팅

--- hi-cc/start.sh
+++ hi-cc/start.sh
@@ -4,4 +4,4 @@ tmux set-option -t $session -p remain-on-exit
 tmux send-keys -t $session C-c
 tmux send-keys -t $session "bash" C-m
 tmux send-keys -t $session "cd $PWD" C-m
-tmux send-keys -t $session "npm run start" C-m
+tmux send-keys -t $session "npm run dev" C-m
