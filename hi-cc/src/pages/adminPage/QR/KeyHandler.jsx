1712e6cddaedfd75b5df991c7c1b900a25f783e9
seoyeon5117 <seoyun020207@gmail.com>
2024-08-18 02:20:07 +0900

    feat: 키핸들러 마진 추가

--- hi-cc/src/pages/adminPage/QR/KeyHandler.jsx
+++ hi-cc/src/pages/adminPage/QR/KeyHandler.jsx
@@ -19,40 +19,38 @@ const KeyHandler = () => {
       },
       credentials: 'include',
     })
-    .then(response => {
-      if(response.ok) {
-        window.location.href = `${VITE_LOCAL_URL}`;
-      } else {
+      .then(response => {
+        if (response.ok) {
+          window.location.href = `${VITE_LOCAL_URL}`;
+        } else {
           throw { status: response.status, message: response.statusText };
-      }
-    })
-    .catch(error => {
-      if (error.status === 403) {
-        setIsKeyValid('false');
-    } else if (error.status === 302) {
-        return;
-    } else if (error.status === 500 || error.status === 502) {
-        navigate('/500');
-    } else {
-        console.error(error);
-    }
-    });
+        }
+      })
+      .catch(error => {
+        if (error.status === 403) {
+          setIsKeyValid('false');
+        } else if (error.status === 302) {
+          return;
+        } else if (error.status === 500 || error.status === 502) {
+          navigate('/500');
+        } else {
+          console.error(error);
+        }
+      });
   }, [key]);
 
   return (
     <Wrapper>
       {isKeyValid === 'loading' ?
-      <InnerWrapper>
-        <h1 style={{margin: '5px 0px'}}>키 처리 중...</h1>
-        <p style={{margin: '5px 0px'}}>잠시만 기다려주세요.</p>
-      </InnerWrapper>
-      : <InnerWrapper>
-        <h1 style={{margin: '5px 0px'}}>만료된 키입니다.</h1>
-        <p style={{margin: '5px 0px'}}>관리자에게 새로운 키를 요청하세요.</p>
-      </InnerWrapper>
-
-}
-      
+        <InnerWrapper>
+          <h1 style={{ margin: '5px 0px' }}>키 처리 중...</h1>
+          <p style={{ margin: '5px 0px' }}>잠시만 기다려주세요.</p>
+        </InnerWrapper>
+        : <InnerWrapper>
+          <h1 style={{ margin: '5px 0px' }}>만료된 키입니다.</h1>
+          <p style={{ margin: '5px 0px' }}>관리자에게 새로운 키를 요청하세요.</p>
+        </InnerWrapper>
+      }
     </Wrapper>
   );
 };
@@ -68,12 +66,13 @@ const Wrapper = styled.div`
   justify-content: center;
   align-items: center;
   text-align: center;
-
   background-color: #F9DBDD;
 `
 
 const InnerWrapper = styled.div`
+  margin: 0 20px;
   padding: 20px 30px;
   background-color: #FAA8B1;
   border-radius: 20px;
+  word-break: keep-all;
 `
