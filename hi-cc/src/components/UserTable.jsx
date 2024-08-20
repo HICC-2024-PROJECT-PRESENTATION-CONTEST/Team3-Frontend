cef2e7f705e6f3b13782f543da8b066973195d7e
seoyeon5117 <seoyun020207@gmail.com>
2024-08-21 03:44:32 +0900

    feat: 관리자 페이지 프로필 삭제 구현

--- hi-cc/src/components/UserTable.jsx
+++ hi-cc/src/components/UserTable.jsx
@@ -1,6 +1,40 @@
+import { useNavigate } from "react-router-dom";
 import styled from "styled-components";
 
+const API_URL = import.meta.env.VITE_API_URL;
+
 export function UserTable({data}) {
+    const navigate = useNavigate();
+
+    function handleDelete(uid) {
+        fetchDeleteProfile(uid);
+    }
+
+    async function fetchDeleteProfile(uid) {
+        fetch(`${API_URL}/profiles/${uid}`, {
+            method: 'DELETE',
+            credentials: 'include',
+        }
+        .then((response) => {
+            if(!response.ok) {
+                throw {status: response.status, message: response.statusText}
+            } else {
+                alert('정상적으로 삭제되었습니다.');
+            }
+        })
+        .catch((error) => {
+            if(error.status === 403) {
+                alert('접근 권한이 없습니다.');
+                navigate('/admin/login');
+            } else if(error.status === 500 || error.status === 502) {
+                navigate('/500');
+            } else {
+                console.error(error);
+            }
+        })
+        )
+    }
+
     return (
         <Result>
             <Td>{data.uid}</Td>
@@ -8,8 +42,12 @@ export function UserTable({data}) {
             <Td>{data.phone}</Td>
             <Td>{data.instagram}</Td>
             <Td>{data.gender}</Td>
-            <Td><button>보기</button></Td>
-            <Td><button>삭제</button></Td>
+            <Td>
+                <Button onClick={() => {}}>보기</Button>
+            </Td>
+            <Td>
+                <Button onClick={() => handleDelete(data.uid)}>삭제</Button>
+            </Td>
         </Result>
     );
 }
@@ -30,4 +68,15 @@ const Td = styled.td`
     white-space: nowrap;
     text-overflow: ellipsis;
     overflow: hidden;
+`
+
+const Button = styled.button`
+    box-sizing: border-box;
+    background-color: #161616;
+    color: #FFFFFF;
+    font-family: sans-serif;
+    border: none;
+    box-shadow: 0 0 1px #FFFFFF;
+    
+    cursor: pointer;
 `
